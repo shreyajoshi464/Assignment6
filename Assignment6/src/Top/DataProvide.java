62297b46a2e18e548fef38af08c0fd6048e8c3e0
shreyajoshi464 <73274630+shreyajoshi464@users.noreply.github.com>
2020-10-26 13:54:50 +0530

    Add files via upload

---
+++ Assignment6/src/Top/DataProvide.java
@@ -0,0 +1,24 @@
+package Top;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DataProvide {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
